fefbf9249f6692233ab471cb1c3f2268015c90d4
darshandeep <darshandeep954@gmail.com>
2025-07-12 16:34:41 +0530

    terf started

---
+++ terraform-aws-roboshop/variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z00631643FZP9GXC6CVLE"
+}
+
+variable "zone_name" {
+    default = "daws84s.xyz"
+}
+
+variable "component" {
+
+}
+
+variable "rule_priority" {
+    
+}
